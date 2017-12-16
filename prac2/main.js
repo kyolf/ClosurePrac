9913f241d3835ec1f60dfcab2393a2161df4f758
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-15 18:48:44 -0800

    got it to print out 0 1 2 instead of 3  3 times

--- prac2/main.js
+++ prac2/main.js
@@ -7,3 +7,10 @@ for(var i = 0; i < 3; i++) {
     console.log(i);
   }, 1000 + i);
 }
+
+//Solution version
+for(var j = 0; j < 3; j++) {
+  setTimeout((function(index) {
+    console.log(index);
+  })(j), 2000);
+}
